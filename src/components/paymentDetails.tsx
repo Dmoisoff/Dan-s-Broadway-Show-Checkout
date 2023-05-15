166e1957d59ea27aa4e4fa33fb0b8e3b32320114
Daniel Moisoff <dmoisoff@gmail.com>
2023-05-14 20:44:37 -0400

    Created payment details component

--- src/components/paymentDetails.tsx
+++ src/components/paymentDetails.tsx
@@ -1,14 +1,78 @@
-import React, { MouseEvent, ReactNode } from "react";
+import React from "react";
+import { UseFormRegister, FieldValues } from "react-hook-form";
+import { formatCard, formatDate } from "~/utils/paymentformating";
 
 interface PaymentDetailsProps {
-
+  register: UseFormRegister<FieldValues>;
 }
 
-const PaymentDetails: React.FC<PaymentDetailsProps> = ({
-
-}) => {
+const PaymentDetails: React.FC<PaymentDetailsProps> = ({ register }) => {
   return (
-
+    <div className="bg-gray row-start-23 col-span-3 col-start-1 row-span-3 rounded-md border-gray-300 bg-green-500 p-4 text-black">
+      <p className="flex justify-around text-2xl font-bold">Payment Details</p>
+      <div className="input_text relative mt-6">
+        {" "}
+        <input
+          type="text"
+          className="h-12 w-full border-2 px-2 pl-7 outline-none transition-all focus:border-blue-900 "
+          placeholder="John Doe"
+          {...register("name", {
+            required: true,
+          })}
+        />{" "}
+        <span className="absolute -top-6 left-0 text-sm">Cardholder Name</span>{" "}
+        <i className="fa fa-user absolute left-2 top-4 text-gray-400"></i>{" "}
+      </div>
+      <div className="input_text relative mt-8">
+        <input
+          type="tel"
+          className="h-12 w-full border-2 px-2 pl-7 outline-none transition-all focus:border-blue-900 "
+          placeholder="0000 0000 0000 0000"
+          maxLength={19}
+          minLength={12}
+          {...register("card", {
+            required: true,
+          })}
+          onChange={formatCard}
+        />
+        <span className="absolute -top-6 left-0 text-sm">Card Number</span>{" "}
+        <i className="fa fa-credit-card absolute left-2 top-[14px] text-sm text-gray-400"></i>{" "}
+      </div>
+      <div className="mt-8 flex gap-5 ">
+        <div className="input_text relative w-full">
+          {" "}
+          <input
+            type="text"
+            className="h-12 w-full border-2 px-2 pl-7 outline-none transition-all focus:border-blue-900 "
+            placeholder="mm/yyyy"
+            data-slots="my"
+            maxLength={7}
+            {...register("expiry", {
+              required: true,
+            })}
+            onChange={formatDate}
+          />{" "}
+          <span className="absolute -top-6 left-0 text-sm">Expiry</span>{" "}
+          <i className="fa fa-calendar absolute left-2 top-4 text-gray-400"></i>{" "}
+        </div>
+        <div className="input_text relative w-full">
+          {" "}
+          <input
+            type="text"
+            className="h-12 w-full border-2 px-2 pl-7 outline-none transition-all focus:border-blue-900 "
+            placeholder="000"
+            data-slots="0"
+            data-accept="\d"
+            maxLength={3}
+            {...register("cvv", {
+              required: true,
+            })}
+          />{" "}
+          <span className="absolute -top-6 left-0 text-sm">CVV</span>{" "}
+          <i className="fa fa-lock absolute left-2 top-4 text-gray-400"></i>{" "}
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ import { useRef } from "react";
 import Button from "~/components/button";
 import { formatDate, formatCard } from "~/utils/paymentformating";
 import SelectShowDetails from "~/components/selectShowDetails";
+import PaymentDetails from "~/components/paymentDetails";
 
 type ShowOption = {
   value: number;
@@ -41,11 +42,6 @@ const Home: NextPage = () => {
     reset({ show: "", ticketQuantity: 0 });
   };
 
-  const dropDownOptions = Object.values(mockShows).map((show) => ({
-    value: show.id,
-    label: `${show.name} - $${show.price}`,
-  }));
-
   const [showInfo, ticketQuantity] = watchedFields;
 
   return (
@@ -53,7 +49,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Mock Checkout Page</title>
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-white">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
         <div className="">
           <h1 className="gap-12 px-4 py-16 text-5xl font-extrabold tracking-tight text-black sm:text-[5rem]">
             Mock <span className="text-[hsl(280,100%,70%)]">Checkout</span> Page
@@ -68,79 +64,8 @@ const Home: NextPage = () => {
               <SelectShowDetails control={control} />
 
               {/* payment details */}
-              <div className="bg-gray row-start-23 col-span-3 col-start-1 row-span-3 rounded-md border-gray-300 bg-green-500 p-4 text-black">
-                <p className="flex justify-around text-2xl font-bold">
-                  Payment Details
-                </p>
-                <div className="input_text relative mt-6">
-                  {" "}
-                  <input
-                    type="text"
-                    className="h-12 w-full border-2 px-2 pl-7 outline-none transition-all focus:border-blue-900 "
-                    placeholder="John Doe"
-                    {...register("name", {
-                      required: true,
-                    })}
-                  />{" "}
-                  <span className="absolute -top-6 left-0 text-sm">
-                    Cardholder Name
-                  </span>{" "}
-                  <i className="fa fa-user absolute left-2 top-4 text-gray-400"></i>{" "}
-                </div>
-                <div className="input_text relative mt-8">
-                  <input
-                    type="tel"
-                    className="h-12 w-full border-2 px-2 pl-7 outline-none transition-all focus:border-blue-900 "
-                    placeholder="0000 0000 0000 0000"
-                    maxLength={19}
-                    minLength={12}
-                    {...register("card", {
-                      required: true,
-                    })}
-                    onChange={formatCard}
-                  />
-                  <span className="absolute -top-6 left-0 text-sm">
-                    Card Number
-                  </span>{" "}
-                  <i className="fa fa-credit-card absolute left-2 top-[14px] text-sm text-gray-400"></i>{" "}
-                </div>
-                <div className="mt-8 flex gap-5 ">
-                  <div className="input_text relative w-full">
-                    {" "}
-                    <input
-                      type="text"
-                      className="h-12 w-full border-2 px-2 pl-7 outline-none transition-all focus:border-blue-900 "
-                      placeholder="mm/yyyy"
-                      data-slots="my"
-                      maxLength={7}
-                      {...register("expiry", {
-                        required: true,
-                      })}
-                      onChange={formatDate}
-                    />{" "}
-                    <span className="absolute -top-6 left-0 text-sm">
-                      Expiry
-                    </span>{" "}
-                    <i className="fa fa-calendar absolute left-2 top-4 text-gray-400"></i>{" "}
-                  </div>
-                  <div className="input_text relative w-full">
-                    {" "}
-                    <input
-                      type="text"
-                      className="h-12 w-full border-2 px-2 pl-7 outline-none transition-all focus:border-blue-900 "
-                      placeholder="000"
-                      data-slots="0"
-                      data-accept="\d"
-                      maxLength={3}
-                      {...register("cvv", {
-                        required: true,
-                      })}
-                    />{" "}
-                    <span className="absolute -top-6 left-0 text-sm">CVV</span>{" "}
-                    <i className="fa fa-lock absolute left-2 top-4 text-gray-400"></i>{" "}
-                  </div>
-                </div>
-              </div>
+              <PaymentDetails register={register} />
+
               {/* summary */}
               <div className="bg-gray col-span-2 col-start-4 row-span-4 row-start-1 rounded-md border-gray-300 bg-blue-500 p-4 text-black">
                 {/* total */}
@@ -148,10 +73,12 @@ const Home: NextPage = () => {
                   <h4>Total</h4>
                   <div>
                     {showInfo && ticketQuantity
-                      ? (mockShows[showInfo?.value]?.price || 0) *
-                          ticketQuantity.value +
-                        (ticketQuantity.value * SERVICE_FEE || SERVICE_FEE) +
-                        PROCESSING_FEE
+                      ? (
+                          (mockShows[showInfo?.value]?.price || 0) *
+                            ticketQuantity.value +
+                          (ticketQuantity.value * SERVICE_FEE || SERVICE_FEE) +
+                          PROCESSING_FEE
+                        ).toFixed(2)
                       : SERVICE_FEE + PROCESSING_FEE}
                   </div>
                 </div>
